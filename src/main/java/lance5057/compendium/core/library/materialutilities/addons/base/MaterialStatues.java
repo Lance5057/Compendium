be042e8c12016cca1c42a54dc0d09cfa20959bf9
Lance5057 <Lance5057@gmail.com>
2022-07-22 13:07:21 -0500

    Added Hand Statue

--- src/main/java/lance5057/compendium/core/library/materialutilities/addons/base/MaterialStatues.java
+++ src/main/java/lance5057/compendium/core/library/materialutilities/addons/base/MaterialStatues.java
@@ -22,17 +22,25 @@ import net.minecraftforge.registries.RegistryObject;
 public class MaterialStatues extends MaterialBase {
 
 	public RegistryObject<ItemDisplayBlock> STATUE_BLOCK;
+	public RegistryObject<ItemDisplayBlock> HAND_STATUE_BLOCK;
 	
 	public RegistryObject<BlockItem> STATUE_ITEMBLOCK;
+	public RegistryObject<BlockItem> HAND_STATUE_ITEMBLOCK;
 	
 	@Override
 	public void setup(MaterialHelper helper) {
 		STATUE_BLOCK = CompendiumBlocks.BLOCKS.register(helper.name + "_statueblock",
 				() -> new ItemDisplayBlock());
+		HAND_STATUE_BLOCK = CompendiumBlocks.BLOCKS.register(helper.name + "_handstatueblock",
+				() -> new ItemDisplayBlock());
+		
 		STATUE_ITEMBLOCK = CompendiumItems.ITEMS.register(helper.name + "_statueitemblock",
 				() -> new BlockItem(STATUE_BLOCK.get(), new Item.Properties().tab(CompendiumItems.GROUP_MATERIALS)));
+		HAND_STATUE_ITEMBLOCK = CompendiumItems.ITEMS.register(helper.name + "_handstatueitemblock",
+				() -> new BlockItem(HAND_STATUE_BLOCK.get(), new Item.Properties().tab(CompendiumItems.GROUP_MATERIALS)));
 		
 		CompendiumTileEntities.displayBlocks.add(STATUE_BLOCK);
+		CompendiumTileEntities.displayBlocks.add(HAND_STATUE_BLOCK);
 	}
 
 	@Override
@@ -46,16 +54,22 @@ public class MaterialStatues extends MaterialBase {
 		model.horizontalBlock(STATUE_BLOCK.get(),
 				model.models().withExistingParent(name + "statue", model.modLoc("block/bases/statue"))
 					.texture("0", model.modLoc("block/material/" + name + "/" + name + "gemblock")));
+		
+		model.horizontalBlock(HAND_STATUE_BLOCK.get(),
+				model.models().withExistingParent(name + "handstatue", model.modLoc("block/bases/handstatue"))
+					.texture("0", model.modLoc("block/material/" + name + "/" + name + "gemblock")));
 	}
 
 	@Override
 	public void registerItemModels(TCItemModels model, String name) {
 		model.forBlockItem(STATUE_ITEMBLOCK, name);
+		model.forBlockItem(HAND_STATUE_ITEMBLOCK, name);
 	}
 
 	@Override
 	public void addTranslations(TCEnglishLoc loc, String capName) {
 		loc.add(STATUE_ITEMBLOCK.get(), capName + " Statue");
+		loc.add(HAND_STATUE_ITEMBLOCK.get(), capName + " Hand");
 	}
 
 	@Override
@@ -73,6 +87,7 @@ public class MaterialStatues extends MaterialBase {
 	@Override
 	public void buildLootTable(BlockLoot table, String name) {
 		table.dropSelf(STATUE_BLOCK.get());
+		table.dropSelf(HAND_STATUE_BLOCK.get());
 	}
 
 	@Override
