ba15574ae44fb3cba772f7a9bd28d0f17148b04e
Lance5057 <Lance5057@gmail.com>
2024-07-16 21:39:42 -0500

    Hammering station

--- src/main/java/com/lance5057/compendium/CompendiumTileEntities.java
+++ src/main/java/com/lance5057/compendium/CompendiumTileEntities.java
@@ -10,11 +10,10 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 public class CompendiumTileEntities {
 	public static final DeferredRegister<BlockEntityType<?>> TILES = DeferredRegister
 			.create(BuiltInRegistries.BLOCK_ENTITY_TYPE, Compendium.MOD_ID);
-
-//	public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<OvenBlockEntity>> OVEN = TILES.register(
-//			"oven", () -> BlockEntityType.Builder.of(OvenBlockEntity::new, ExtraDelightBlocks.OVEN.get()).build(null));
 	
 	public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<WorkbenchBlockEntity>> WORKSTATION_TE = TILES.register(
 			"workbench", () -> BlockEntityType.Builder.of(WorkbenchBlockEntity::new, CompendiumBlocks.WORKBENCH.get()).build(null));
+	
+	
 
 }
--- src/main/java/com/lance5057/compendium/recipes/interfaces/item/io/single/IRecipeSingleItemIn.java
+++ src/main/java/com/lance5057/compendium/recipes/interfaces/item/io/single/IRecipeSingleItemIn.java
@@ -1,7 +1,7 @@
 package com.lance5057.compendium.recipes.interfaces.item.io.single;
 
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
 
 public interface IRecipeSingleItemIn {
-	public void setItemIn(ItemStack in);
+	public Ingredient getItemIn();
 }
