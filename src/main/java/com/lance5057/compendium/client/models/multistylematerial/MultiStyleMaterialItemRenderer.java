cc26cc7588146408179f4f63c7e23d0d671a8c60
Lance5057 <Lance5057@gmail.com>
2025-06-27 13:46:36 -0500

    chair item renderer working!

--- src/main/java/com/lance5057/compendium/client/models/multistylematerial/MultiStyleMaterialItemRenderer.java
+++ src/main/java/com/lance5057/compendium/client/models/multistylematerial/MultiStyleMaterialItemRenderer.java
@@ -3,7 +3,6 @@ package com.lance5057.compendium.client.models.multistylematerial;
 import java.util.List;
 
 import org.jetbrains.annotations.Nullable;
-import org.joml.Quaternionf;
 
 import com.lance5057.compendium.CompendiumComponents;
 import com.lance5057.compendium.client.models.multimaterial.MultiMaterialModelData;
@@ -11,16 +10,19 @@ import com.lance5057.compendium.client.models.style.StyleModelData;
 import com.lance5057.compendium.components.block.MultiMaterialBlockComponent;
 import com.lance5057.compendium.components.block.StyleBlockComponent;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.MatrixUtil;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.geom.EntityModelSet;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.client.ClientHooks;
 import net.neoforged.neoforge.client.model.data.ModelData;
 import net.neoforged.neoforge.client.model.data.ModelData.Builder;
 import net.neoforged.neoforge.client.model.renderable.BakedModelRenderable;
@@ -37,19 +39,6 @@ public abstract class MultiStyleMaterialItemRenderer extends BlockEntityWithoutL
 	public void renderByItem(ItemStack stack, ItemDisplayContext displayContext, PoseStack ps, MultiBufferSource mbs,
 			int packedLight, int overlay) {
 		ps.pushPose();
-
-		if (displayContext == ItemDisplayContext.GUI) {
-
-			ps.mulPose(new Quaternionf().rotateXYZ((float) Math.toRadians(30), (float) Math.toRadians(225), 0));
-			ps.translate(-0.05f, 1.0f, -1.9f);
-			ps.scale(1.25f, 1.25f, 1.25f);
-		} else if (displayContext == ItemDisplayContext.FIRST_PERSON_LEFT_HAND
-				|| displayContext == ItemDisplayContext.FIRST_PERSON_RIGHT_HAND) {
-			ps.translate(0, 0.35, -0.17f);
-		} else {
-			ps.translate(0, 0.25f, 0f);
-		}
-
 		if (stack.getItem() instanceof BlockItem bi) {
 			BakedModel bm = Minecraft.getInstance().getBlockRenderer().getBlockModel(bi.getBlock().defaultBlockState());
 
@@ -65,7 +54,14 @@ public abstract class MultiStyleMaterialItemRenderer extends BlockEntityWithoutL
 			if (s != null)
 				md.with(StyleModelData.STYLES, getStyles(s.styles()));
 
-			BakedModelRenderable.of(bm).withContext(md.build()).render(ps, mbs, null, packedLight, overlay, overlay, null);
+			bm = ClientHooks.handleCameraTransforms(ps, bm, displayContext, true);
+
+			BakedModelRenderable bmr = BakedModelRenderable.of(bm);
+			if (bmr != null) {
+				bmr.withContext(md.build()).render(ps, mbs, texture -> RenderType.entityCutout(texture), packedLight,
+						overlay, overlay, null);
+
+			}
 		}
 
 		ps.popPose();
