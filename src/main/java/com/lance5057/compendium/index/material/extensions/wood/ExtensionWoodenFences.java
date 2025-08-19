d4ab38410bb0ed789c569a22e264d477d305e366
Lance5057 <Lance5057@gmail.com>
2025-08-19 00:49:21 -0500

    i dunno

---
+++ src/main/java/com/lance5057/compendium/index/material/extensions/wood/ExtensionWoodenFences.java
@@ -0,0 +1,85 @@
+package com.lance5057.compendium.index.material.extensions.wood;
+
+import com.lance5057.compendium.data.IndexBlockModelProvider;
+import com.lance5057.compendium.index.material.base._MaterialBase;
+import com.lance5057.compendium.index.material.extensions._MaterialExtension;
+
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.data.recipes.RecipeOutput;
+import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.world.item.CreativeModeTab.Output;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
+import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
+import net.neoforged.neoforge.common.data.BlockTagsProvider;
+import net.neoforged.neoforge.common.data.LanguageProvider;
+
+public class ExtensionWoodenFences extends _MaterialExtension {
+
+	@Override
+	public void setup(_MaterialBase base) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void tab(_MaterialBase base, Output output) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void blockStateModel(_MaterialBase base, BlockStateProvider bsp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void blockModel(_MaterialBase base, IndexBlockModelProvider ibmp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void itemModel(_MaterialBase base, ItemModelProvider tmp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void engLoc(_MaterialBase base, LanguageProvider lp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void recipes(_MaterialBase base, RecipeOutput consumer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void blockLoot(_MaterialBase base, BlockLootSubProvider blp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setupItemTags(_MaterialBase base, ItemTagsProvider itp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setupBlockTags(_MaterialBase base, BlockTagsProvider itp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setupClient(_MaterialBase base, FMLClientSetupEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
