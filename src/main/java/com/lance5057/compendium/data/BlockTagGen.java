52116121ecf53583b964a36172416095a7ca2132
Lance5057 <Lance5057@gmail.com>
2026-01-03 18:45:56 -0600

    block tags

--- src/main/java/com/lance5057/compendium/data/BlockTagGen.java
+++ src/main/java/com/lance5057/compendium/data/BlockTagGen.java
@@ -6,6 +6,7 @@ import org.jetbrains.annotations.Nullable;
 
 import com.lance5057.compendium.CompendiumBlocks;
 import com.lance5057.compendium.CompendiumTags;
+import com.lance5057.compendium.index.CompendiumIndex;
 
 import net.minecraft.core.HolderLookup.Provider;
 import net.minecraft.data.PackOutput;
@@ -26,10 +27,14 @@ public class BlockTagGen extends BlockTagsProvider {
 		tag(CompendiumTags.PRYABLE).add(Blocks.IRON_BLOCK);
 
 		tag(CompendiumTags.TABLE).add(CompendiumBlocks.TABLE.get(), CompendiumBlocks.CLOTHED_TABLE.get());
-		
+
 		tag(BlockTags.FENCES).add(CompendiumBlocks.FANCY_FENCE.get());
-		
+
 		tag(BlockTags.BEDS).add(CompendiumBlocks.FANCY_BED.get());
+
+		CompendiumIndex.index.forEach(i -> {
+			i.setupBlockTags(this);
+		});
 	}
 
 }
