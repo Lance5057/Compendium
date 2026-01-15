2476c22ae540f2d630773a8fecaf438463737460
Lance5057 <Lance5057@gmail.com>
2026-01-15 13:38:01 -0600

    Create CompendiumCommands.java

---
+++ src/main/java/com/lance5057/compendium/commands/CompendiumCommands.java
@@ -0,0 +1,28 @@
+package com.lance5057.compendium.commands;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
+import com.mojang.brigadier.tree.LiteralCommandNode;
+
+import net.minecraft.commands.CommandBuildContext;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
+
+public class CompendiumCommands {
+	public static void register(CommandDispatcher<CommandSourceStack> dispatcher, CommandBuildContext commandBuildContext) {
+		LiteralArgumentBuilder<CommandSourceStack> builder = Commands.literal("compendium")
+				.executes(CompendiumCommands::run).then(EachStyleBuildCommand.register(commandBuildContext));
+		
+		LiteralCommandNode<CommandSourceStack> node = dispatcher.register(builder);
+        dispatcher.register(Commands.literal("comp").executes(CompendiumCommands::run).redirect(node));
+	}
+
+	private static int run(CommandContext<CommandSourceStack> ctx) throws CommandSyntaxException {
+		throw new SimpleCommandExceptionType(Component.translatable("commands.compendium.usage", ctx.getInput()))
+				.create();
+	}
+}
